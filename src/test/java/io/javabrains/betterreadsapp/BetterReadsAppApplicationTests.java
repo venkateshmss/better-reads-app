4e53edc952bffe628de43d9a61d14dff0b5d2e53
Mounika Jeganathan <mounika.jothi@gmail.com>
2024-04-23 14:29:01 -0700

    Created the base pack of Spring boot app with required dependencies

---
+++ src/test/java/io/javabrains/betterreadsapp/BetterReadsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.betterreadsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
